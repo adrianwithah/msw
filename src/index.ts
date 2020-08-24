f24b86f9425f156eee0f036d78f504a82eb0db66
Artem Zakharchenko <kettanaito@gmail.com>
2020-08-24 09:17:41 +0200

    Adds basic classes for WebSocket support

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,9 @@ export {
   GraphQLResponseResolver,
   GraphQLRequestParsedResult,
 } from './graphql'
+export { ws } from './ws/ws'
+
+/* Utilities */
 export { matchRequestUrl } from './utils/matching/matchRequestUrl'
 
 /* Utils */
